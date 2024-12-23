e4f4f9eeada39db64df5fbe6a73661a46cf9541f
master <bryngowi2@gmail.com>
2024-12-23 18:18:35 +0300

    version1

--- app/src/main/java/com/example/hotelmanagement/Registration.java
+++ app/src/main/java/com/example/hotelmanagement/Registration.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,7 @@ public class Registration extends AppCompatActivity {
          mvtolog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(Registration.this, "Login", Toast.LENGTH_SHORT).show();
                 Intent intent= new Intent(Registration.this, Login.class);
                 startActivity(intent);
             }
