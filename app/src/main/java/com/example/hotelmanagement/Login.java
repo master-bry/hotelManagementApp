c830430a32938e23cc6007e6eb2b2a25cd16fdb6
master <bryngowi2@gmail.com>
2024-12-28 18:39:27 +0300

    version1

--- app/src/main/java/com/example/hotelmanagement/Login.java
+++ app/src/main/java/com/example/hotelmanagement/Login.java
@@ -12,18 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 /** @noinspection ALL */
 public class Login extends AppCompatActivity {
     Button mvtoreg;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_activitylogin);
+        login = findViewById(R.id.button_login);
         mvtoreg = findViewById(R.id.mvregister);
         mvtoreg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Login.this, "Register", Toast.LENGTH_SHORT).show();
                 Intent intent= new Intent(Login.this, Registration.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this, "Login", Toast.LENGTH_SHORT).show();
+                Intent intent= new Intent(Login.this, Home.class);
                 startActivity(intent);
             }
         });
